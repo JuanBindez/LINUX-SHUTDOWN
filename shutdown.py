d160078f90e3409da43ff9c03f148c34a99a2a75
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-03-01 23:54:06 -0300

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -1,8 +1,16 @@
-#python3 shutdown.py
-
 import os
 
-#................... FUNÇÕES....................
+
+class Color():
+    VERDE = '\033[92m'
+    VERMELHO = '\033[91m'
+    AMARELO = '\033[93m'
+    AZUL = '\033[1;34m'
+    MAGENTA = '\033[1;35m'
+    VERDE_CLARO = '\033[1;92m'
+    NEGRITO = '\033[;1m'
+    RESET = '\033[0m'
+
 
 def desliga1():
     os.system("sudo shutdown -h +1")
@@ -22,20 +30,23 @@ def desliga60():
 def desliga120():
     os.system("sudo shutdown -h +120")
 
-# ................FIM DAS FUNÇÕES.................
 
-
-
-#................INICIO DO PROGRAMA...............
-
-print("[1] 1 MIN")
+print(Color.AMARELO + "[1] 1 MIN")
 print("[5] 5 MIN")
 print("[10] 10 MIN")
 print("[20] 20 MIN")
 print("[60] 1 HORA")
 print("[120] 2 HORAS")
 
-id1 = int(input("PARA AGENDAR O DESLIGAMENTO DO SEU PC, DIGITE O NUMERO CORRESPONDENTE:"))
+
+try:
+    id1 = int(input(Color.VERDE + "PARA AGENDAR O DESLIGAMENTO DO SEU PC, DIGITE O NUMERO CORRESPONDENTE:"))
+
+except ValueError:
+    print("digite apenas numeros")
+
+except NameError:
+    print("digite apenas numeros")
 
 
 if id1 == 1:
@@ -55,6 +66,3 @@ if id1 == 60:
 
 if id1 == 120:
     desliga120()
-
-# ................FIM DO PROGRAMA.................
-
